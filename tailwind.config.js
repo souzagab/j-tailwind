024d053bf61bc4b2a1b92d0921dcf92d9d23ca85
Gabriel S <thesouzagab@gmail.com>
2021-08-07 16:33:01 -0300

    chore: add tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_**/*.html',
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
